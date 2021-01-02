83983d93c966c2b2db439f5f4959b6789b31389b
Seth W <swidoff@gmail.com>
2021-01-01 20:42:59 -0800

    day 22 checkpoint. try again.

--- src/day22.rs
+++ src/day22.rs
@@ -59,108 +59,139 @@ const SPELLS: [Spell; 5] = [
     },
 ];
 
-fn win(
-    hp1_initial: i64,
-    mana1_initial: i64,
-    hp2_initial: i64,
+#[derive(Copy, Clone)]
+struct State {
+    hp1: i64,
+    mana1: i64,
+    hp2: i64,
     dam2: i64,
-    spells: &Vec<usize>,
-) -> (bool, i64) {
-    let mut effects: [u8; SPELLS.len()] = [0; SPELLS.len()];
-    let mut hp1 = hp1_initial;
-    let mut mana1 = mana1_initial;
-    let mut hp2 = hp2_initial;
-    let mut turn = 0;
-    let mut cost = 0;
-    loop {
-        let mut arm1 = 0;
-        for i in 0..SPELLS.len() {
-            if effects[i] > 0 {
-                let spell = &SPELLS[i];
-                println!("EFFECT {} {}", spell.id, effects[i]);
-                hp2 -= spell.damage;
-                mana1 += spell.mana;
-                hp1 += spell.heal;
-                arm1 += spell.armor;
-                effects[i] -= 1;
-            }
-        }
-
-        if hp2 <= 0 {
-            return (true, cost);
-        }
-
-        if turn % 2 == 0 {
-            let i = turn / 2;
-            if i < spells.len() {
-                let spell_index = spells[i];
-                let spell = &SPELLS[spell_index];
-                if mana1 >= spell.cost {
-                    println!("CAST {}", spell.id);
-                    mana1 -= spell.cost;
-                    cost += spell.cost;
-                    if spell.turns == 0 {
-                        hp2 -= spell.damage;
-                        hp1 += spell.heal
-                    } else if effects[spell_index] == 0 {
-                        effects[spell_index] = spell.turns;
-                    } else {
-                        panic!("Spell still in effect!")
-                    }
-                } else {
-                    panic!("Not enough mana!")
-                }
-            }
-            if hp2 <= 0 {
-                return (true, cost);
-            }
-        } else {
-            hp1 -= if dam2 > arm1 { dam2 - arm1 } else { 1 };
-
-            if hp1 <= 0 {
-                return (false, cost);
-            }
-        }
-        println!(
-            "turn: {}, hp1: {}, hp2: {}, mana1: {}",
-            turn, hp1, hp2, mana1
-        );
-        turn += 1;
-    }
+    effects: [u8; SPELLS.len()],
+    cost: i64,
 }
 
-fn spell_combinations(max_spells: usize) -> Vec<Vec<usize>> {
-    let mut res = Vec::new();
-
-    let combinations = (0..SPELLS.len())
-        .combinations_with_replacement(max_spells)
-        .flat_map(|s| s.into_iter().permutations(max_spells).unique());
-    for c in combinations {
-        res.push(c);
+impl State {
+    fn new(hp1: i64, mana1: i64, hp2: i64, dam2: i64) -> State {
+        State {
+            hp1,
+            mana1,
+            hp2,
+            dam2,
+            effects: [0; SPELLS.len()],
+            cost: 0,
+        }
     }
 
-    res.sort_by_key(|v| v.iter().map(|i| SPELLS[*i].cost).sum::<i64>());
-    res
+    fn player_won(&self) -> bool {
+        self.hp2 <= 0
+    }
+
+    fn boss_won(&self) -> bool {
+        self.hp1 <= 0
+    }
+
+    fn play(&self, spell_index: usize) -> State {
+        let mut s = self.clone();
+        for turn in 0..2 {
+            let mut arm1 = 0;
+            if s.hp2 > 0 {
+                for i in 0..SPELLS.len() {
+                    if s.effects[i] > 0 {
+                        let spell = &SPELLS[i];
+                        arm1 += spell.armor;
+                        s.hp2 -= spell.damage;
+                        s.mana1 += spell.mana;
+                        s.hp1 += spell.heal;
+                        s.effects[i] -= 1;
+                    }
+                }
+            }
+
+            if s.hp1 > 0 && s.hp2 > 0 {
+                if turn % 2 == 0 {
+                    let spell = &SPELLS[spell_index];
+                    if s.mana1 >= spell.cost {
+                        s.mana1 -= spell.cost;
+                        s.cost += spell.cost;
+                        if spell.turns == 0 {
+                            s.hp2 -= spell.damage;
+                            s.hp1 += spell.heal
+                        } else if s.effects[spell_index] == 0 {
+                            s.effects[spell_index] = spell.turns;
+                        } else {
+                            panic!("Spell still in effect!")
+                        }
+                    } else {
+                        panic!("Not enough mana!")
+                    }
+                } else {
+                    s.hp1 -= if s.dam2 > arm1 { s.dam2 - arm1 } else { 1 };
+                }
+            }
+        }
+
+        s
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day22::{spell_combinations, win};
+    use crate::day22::State;
 
     #[test]
-    fn test_part1_example() {
-        assert_eq!(true, win(10, 250, 13, 8, &vec![3, 0]).0);
-        assert_eq!(true, win(10, 250, 14, 8, &vec![4, 2, 1, 3, 0]).0);
+    fn test_part1_example1() {
+        let mut state = State::new(10, 250, 13, 8);
+        state = state.play(3);
+        assert_eq!(2, state.hp1);
+        assert_eq!(10, state.hp2);
+        assert_eq!(5, state.effects[3]);
+        assert_eq!(77, state.mana1);
+
+        state = state.play(0);
+        assert_eq!(2, state.hp1);
+        assert_eq!(0, state.hp2);
+        assert_eq!(3, state.effects[3]);
+        assert_eq!(24, state.mana1);
+        assert_eq!(true, state.player_won());
     }
 
     #[test]
-    fn test_part1() {
-        // let spells = vec![2, 4, 3, 2, 4, 3, 2, 4, 3, 2, 4, 3, 2]; 2173
-        // let spells = vec![2, 4, 3, 2, 4, 3, 2, 4, 3, 2, 4, 3, 0]; // 2113
-        // let spells = vec![2, 4, 3, 2, 4, 3, 2, 4, 3, 2, 0, 0, 3]; // 1937
-        // let spells = vec![2, 4, 3, 2, 4, 3, 2, 4, 3, 2, 0, 3]; // 1884
-        let spells = vec![2, 4, 3, 2, 4, 3, 2, 4, 3, 2, 0, 3];
-        let (win, cost) = win(50, 500, 71, 10, &spells);
-        println!("win: {}, cost: {}", win, cost);
+    fn test_part1_example2() {
+        let mut state = State::new(10, 250, 14, 8);
+        state = state.play(4);
+        assert_eq!(2, state.hp1);
+        assert_eq!(14, state.hp2);
+        assert_eq!(4, state.effects[4]);
+        assert_eq!(122, state.mana1);
+
+        state = state.play(2);
+        assert_eq!(1, state.hp1);
+        assert_eq!(14, state.hp2);
+        assert_eq!(2, state.effects[4]);
+        assert_eq!(211, state.mana1);
+
+        state = state.play(1);
+        assert_eq!(2, state.hp1);
+        assert_eq!(12, state.hp2);
+        assert_eq!(3, state.effects[2]);
+        assert_eq!(0, state.effects[4]);
+        assert_eq!(340, state.mana1);
+
+        state = state.play(3);
+        assert_eq!(1, state.hp1);
+        assert_eq!(9, state.hp2);
+        assert_eq!(1, state.effects[2]);
+        assert_eq!(5, state.effects[3]);
+        assert_eq!(167, state.mana1);
+
+        state = state.play(0);
+        assert_eq!(1, state.hp1);
+        assert_eq!(-1, state.hp2);
+        assert_eq!(0, state.effects[2]);
+        assert_eq!(3, state.effects[3]);
+        assert_eq!(114, state.mana1);
+        assert_eq!(true, state.player_won());
     }
+
+    #[test]
+    fn test_part1() {}
 }
